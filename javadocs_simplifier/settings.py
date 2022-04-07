bd980cf15c157c5cae2fded277a69d75f567cc89
M K <85588877+mkpro118@users.noreply.github.com>
2022-04-08 03:50:48 +0530

    Update settings.py

--- javadocs_simplifier/settings.py
+++ javadocs_simplifier/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vxvk3i&&#wsr+c%8l_oy^4$sdw#gvp*1)6lr^eskr(6nrty_n='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["*", ]
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
